10cf2193dcecce0147d10f8d60fcb1c447857893
JewelChidinma <jewelchidinma@gmail.com>
2025-06-12 16:05:32 +0100

    completed password authentication

--- authenticate.js
+++ authenticate.js
@@ -3,6 +3,19 @@ const path = require("path")
 
 const userDbPath = path.join(__dirname, "db", "users.json")
 
+function getAllUsers() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(userDbPath, "utf8", (err, users) => {
+            if (err) {
+                reject(err)
+            }
+
+            resolve(JSON.parse(users))
+
+        })
+    })
+}
+
 function authenticate(req, res) {
     return new Promise((resolve, reject) => {
         const body = []
@@ -10,13 +23,31 @@ function authenticate(req, res) {
         req.on("data", (chunk) => {
             body.push(chunk)
         })
-        req.on("end", () => {
+        req.on("end", async () => {
             const parsedBody = Buffer.concat(body).toString()
             console.log(parsedBody)
 
             if (!parsedBody) {
                 reject("No username or password entered")
             }
+
+            const loginDetails = JSON.parse(parsedBody)
+            console.log(loginDetails)
+
+            const users = await getAllUsers()
+            const userFound = users.find((user) => {
+                return user.username === loginDetails.username
+            })
+
+            if (!userFound) {
+                reject("User not found! Please sign up.")
+            }
+
+            if (userFound.password !== loginDetails.password) {
+                reject("Invalid username or password")
+            }
+
+            resolve()
         })
 
     })
